4f294396430654378305a0726d4d62576e0d0cd2
thuyamyint <thuyamyint2022@gmail.com>
2025-07-21 14:21:18 +0630

    a yan chit tl arrbwrr mwa

--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -9,32 +9,37 @@ const LoginForm = () => {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [loading, setLoading] = useState(false)
-    const emailRegex = "/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;"
+    const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+
 
     const userLogin = async () => {
         if (!username || username.trim() === "") {
             return alert("Invalid username!")
-        } else if (!email || email.trim() === "") {
+        } else if (!emailRegex.test(email)) {
             return alert("Invalid email!")
         } else if (!password || password.trim() === "") {
             return alert("Invalid password!")
         }
         try {
+            setLoading(true)
             const response = await axios.post('https://quickserve-t1f6.onrender.com/api/v1/user/register', {
-                name:username,
+                name: username,
                 email,
                 password
             })
-            console.log("user login ", response)
+            if (response.success)
+                alert(response?.data?.message)
+
         } catch (error) {
-            console.log("Error login!", error)
+            console.log("Error login!", error?.response?.data?.message)
+            alert(error?.response?.data?.message)
         }
     }
     return (
         <div className='w-screen h-screen bg flex justify-center items-center'>
             <div className='blurr p-8 rounded-2xl flex flex-col items-center shadow-xl shadow-black/40'>
                 <div className='text-4xl font-bold text-white mb-8'>Login</div>
-                <form className='flex flex-col gap-6' onSubmit={(e) => {
+                <form className={`flex flex-col gap-6 ${loading && 'opacity-50'}`} onSubmit={(e) => {
                     e.preventDefault()
                     userLogin()
                 }}>
@@ -42,14 +47,14 @@ const LoginForm = () => {
                         <input value={username}
                             onChange={(e) => {
                                 setUsername(e.target.value)
-                            }} type="text" placeholder='Username' className='text-white placeholder:text-white text-xl outline-0 ' />
+                            }} type="text" placeholder='Username' className='w-[90%] text-white placeholder:text-white text-xl outline-0 ' />
                         <FaUser className='text-white text-xl' />
                     </div>
                     <div className='flex items-center border-2 border-white rounded-full   md:w-[400px] w-[300px]  px-4 py-2 justify-between'>
                         <input value={email}
                             onChange={(e) => {
                                 setEmail(e.target.value)
-                            }} type="text" placeholder='Email' className='text-white placeholder:text-white text-xl outline-0 ' />
+                            }} type="text" placeholder='Email' className='w-[90%] text-white placeholder:text-white text-xl outline-0 ' />
                         <IoMail className='text-white text-xl' />
                     </div>
                     <div>
@@ -57,7 +62,7 @@ const LoginForm = () => {
                             <input value={password}
                                 onChange={(e) => {
                                     setPassword(e.target.value)
-                                }} type="password" placeholder='Password' className='text-white placeholder:text-white text-xl outline-0 ' />
+                                }} type="password" placeholder='Password' className='w-[90%] text-white placeholder:text-white text-xl outline-0 ' />
                             <MdOutlineKey className='text-white text-2xl' />
                         </div>
                         <div className='flex justify-between text-white mt-4 p-2'>
@@ -65,10 +70,13 @@ const LoginForm = () => {
                             <button className='underline'>Forget Password</button>
                         </div>
                     </div>
-                    <button className='bg-white font-bold p-2 rounded-full text-xl'>Login</button>
+                    <button className='bg-white font-bold p-2 rounded-full text-xl active:opacity-70'>
+                        {
+                            loading ? 'Loading' : 'Register'
+                        }</button>
                     <div className='flex items-center justify-center text-white gap-1'>
-                        <div>Don't have an account?</div>
-                        <div className='font-bold'>Register</div>
+                        <div>Already have an account?</div>
+                        <div className='font-bold'>Login</div>
                     </div>
                 </form>
             </div>
